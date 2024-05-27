6c21acac3c65f8748e53df1dcb75519383a45402
franklin najera <najerafranklin2003@gmail.com>
2024-05-27 18:05:15 -0400

    Initial Commit

---
+++ batmankode1/batmankode1App.swift
@@ -0,0 +1,17 @@
+//
+//  batmankode1App.swift
+//  batmankode1
+//
+//  Created by Adrian Najera on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct batmankode1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
