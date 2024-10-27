20c0821706040caa5ff63139997c28ce715e9395
Adry020304 <najerafranklin2003@gmail.com>
2024-10-27 19:10:34 -0400

    save

--- batmankode1/ContentView.swift
+++ batmankode1/ContentView.swift
@@ -7,27 +7,63 @@
 
 import SwiftUI
         
-struct List_Selection_Single: View {
-    @State private var data = ["Practice Coding","Big project","Simple project","strong project","My bussiness","Clean house","Real project"]
-    @State private var selection : string?
-    
-    var body : some View {
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    .font(.title)
-    }
+struct todo: Identifiable{
+    let id = UUID()
+    var action = ""
+    var due = ""
+    var isIndented = false
 }
-    #Preview{
-        List_MoveRow()
+struct List_ListRowbackground: View {
+    @State private var neWToDo = ""
+    
+    @State var data = [
+        todo(action: "Practice coding",due:"Today"),
+        todo(action:"Grocery shopping",due: "Today"),
+        todo(action:"Get tickets",due: "Tomorrow"),
+        todo(action:"Do laundry",due: "Next week"),
+        todo(action:"Cook dinner",due: "Next week")
+    ]
+    var body: some View {
+        List{
+            Section{
+                ForEach(data) { datum in
+                    Text(datum.action)
+                        .font(Font.system(size: 24))
+                        .foregroundStyle(self.getTextColor(due:datum.due))
+                        .listRowBackground(datum.due == "today" ? Color.green: Color.clear)
+                        .padding()
+                }
+            }header:{
+                VStack {
+                    Text("To do").font(.title)
+                    HStack{
+                        TextField("new todo", text: $neWToDo)
+                            .textFieldStyle(.roundedBorder)
+                        Button(action:{
+                            data.append(todo(action:neWToDo))
+                            neWToDo = ""
+                        }) {
+                            Image(systemName: "plus.circle.fill").font(.title)
+                                }
+                            }
+                        }
+                                .padding(.bottom)
+                  }
+              }
+                .listStyle(.plain)
+            }
+                                  private func getTextColor(due:String) -> Color {
+                                due == "Today" ? Color.black : Color.primary
+                            }
+                        }
+        #Preview{
+            List_ListRowbackground()
         }
-   
+
+
+
+
+
+
+
 
