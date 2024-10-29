2e3ab5aa8cf11c69805050a8c5297a82fb7003e8
Adry020304 <najerafranklin2003@gmail.com>
2024-10-28 22:46:00 -0400

    save

--- batmankode1/ContentView.swift
+++ batmankode1/ContentView.swift
@@ -7,57 +7,36 @@
 
 import SwiftUI
         
-struct todo: Identifiable{
-    let id = UUID()
-    var action = ""
-    var due = ""
-    var isIndented = false
-}
-struct List_ListRowbackground: View {
-    @State private var neWToDo = ""
+struct Parent: Identifiable {
     
-    @State var data = [
-        todo(action: "Practice coding",due:"Today"),
-        todo(action:"Grocery shopping",due: "Today"),
-        todo(action:"Get tickets",due: "Tomorrow"),
-        todo(action:"Do laundry",due: "Next week"),
-        todo(action:"Cook dinner",due: "Next week")
-    ]
+    var id = UUID()
+    var name = ""
+    var children: [Parent]?
+}
+struct List_WithChildren: View {
+    var parents = [Parent(name: "Mark",
+                          children: [Parent(name: "Beatriz")]),
+                   Parent(name: "Rodrigo",
+                   children: [Parent(name:"Kai"),Parent(name:"Brendan"),
+                              Parent(name:"Easton")]),
+                   
+                   Parent(name: "Marcela",
+                   children:[Parent(name:"sam"), Parent(name:"Melissa"),
+                             Parent(name:"Melany")])]
     var body: some View {
-        List{
-            Section{
-                ForEach(data) { datum in
-                    Text(datum.action)
-                        .font(Font.system(size: 24))
-                        .foregroundStyle(self.getTextColor(due:datum.due))
-                        .listRowBackground(datum.due == "today" ? Color.green: Color.clear)
-                        .padding()
-                }
-            }header:{
-                VStack {
-                    Text("To do").font(.title)
-                    HStack{
-                        TextField("new todo", text: $neWToDo)
-                            .textFieldStyle(.roundedBorder)
-                        Button(action:{
-                            data.append(todo(action:neWToDo))
-                            neWToDo = ""
-                        }) {
-                            Image(systemName: "plus.circle.fill").font(.title)
-                                }
-                            }
-                        }
-                                .padding(.bottom)
-                  }
-              }
-                .listStyle(.plain)
+        VStack(spacing: 20.0){
+            Text("List")
+            Text("Children")
+            Text("Never give up in my life is only my and my mind")
+            List(parents, children: \.children) {parent in
+                Text("\(parent.name)")
             }
-                                  private func getTextColor(due:String) -> Color {
-                                due == "Today" ? Color.black : Color.primary
-                            }
-                        }
+        }
+        .font(.title)
+    }
+}
         #Preview{
-            List_ListRowbackground()
+            List_WithChildren()
         }
 
 
