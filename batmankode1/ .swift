1b33a07f229d2fa88de4ff09f4e8a52782a947fc
Adry020304 <najerafranklin2003@gmail.com>
2024-11-11 22:08:50 -0500

    save

--- batmankode1/ .swift
+++ batmankode1/ .swift
@@ -7,32 +7,40 @@
 
 import SwiftUI
         
-struct List_RowSpacing: View{
+struct List_ListSectionSeparator: View{
     var body: some View {
-        VStack(spacing: 0){
-            
-            List {
-                Text("Row 1")
-                Text("Row 2")
-                    .listRowBackground(Color.red)
-                Text("Row 3")
-                    .listRowBackground(Color.blue)
-                Text("Row 4")
-                    .listRowBackground(Color.green)
-                Text("Row 5")
-                    .listRowBackground(Color.pink)
-            }
-            
-            .listStyle(.plain)
-            .listRowSpacing(100)
-        }
         
-        .font(.title)
+        VStack(spacing: 0){
+            Text("List")
+            Text("Never in the life is heasy but you can do it")
+                .background(Color.cyan)
+            List {
+                Section {
+                    
+                    Text("Row 1")
+                    Text("Row 2")
+                    Text("Row 3")
+                    Text("Row 4")
+                    Text("Row 5")
+                    Text("Row 6")
+                } header: {
+                    Text("Section 1")
+                }
+                Section {
+                    Text("Row 7")
+                    Text("Row 8")
+                    Text("Row 9")
+                    Text("Row 10")
+                } header: {
+                    Text("Section 2")
+                }
+            }
+            .listStyle(.plain)
+        }
     }
 }
-
 #Preview {
-    List_RowSpacing()
+    List_ListSectionSeparator()
      }
 
 
