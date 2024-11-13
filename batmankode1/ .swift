4b5b791a800f994ec3841734f1e51a11e28b93a7
Adry020304 <najerafranklin2003@gmail.com>
2024-11-12 21:36:12 -0500

    save

--- batmankode1/ .swift
+++ batmankode1/ .swift
@@ -7,40 +7,37 @@
 
 import SwiftUI
         
-struct List_ListSectionSeparator: View{
+struct List_ListRowSeparatorTint: View{
     var body: some View {
         
-        VStack(spacing: 0){
+        VStack(spacing: 20.0) {
             Text("List")
-            Text("Never in the life is heasy but you can do it")
+            Text("In my project is very important my coding")
                 .background(Color.cyan)
+            
             List {
-                Section {
-                    
-                    Text("Row 1")
-                    Text("Row 2")
-                    Text("Row 3")
-                    Text("Row 4")
-                    Text("Row 5")
-                    Text("Row 6")
-                } header: {
-                    Text("Section 1")
-                }
-                Section {
-                    Text("Row 7")
-                    Text("Row 8")
-                    Text("Row 9")
-                    Text("Row 10")
-                } header: {
-                    Text("Section 2")
-                }
+                Text("Row 1")
+                Text("Yellow")
+                    .listRowSeparatorTint(.yellow)
+                Text("Row 2")
+                Text("Row 3")
+                Text("Row 4")
+                Text("Row 6")
+                Text("Green on top")
+                    .listRowSeparatorTint(.green, edges: .top)
+                Text("Row 8")
+                Text("Row 9")
+                Text("Row 10")
             }
-            .listStyle(.plain)
         }
+        .font(.title)
     }
 }
+        
+        
+       
 #Preview {
-    List_ListSectionSeparator()
+    List_ListRowSeparatorTint()
      }
 
 
